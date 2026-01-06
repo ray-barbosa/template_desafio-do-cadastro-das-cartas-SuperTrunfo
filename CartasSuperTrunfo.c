f417c2a91d7fd09a7d5ca35e901de320ea13d768
Ray Krishna <rayanebarbosa.dev@gmail.com>
2026-01-06 19:55:47 +0000

    feature: Super trunfo em C - Nivel novato
    Com esse programa o usuario podera cadastras 2 cartas ao executar o programa usando sera nescessario que o usuario interaja por meio do teclado, adicionando as entradas conforme solicitado para gerar ao final uma visualzação da carta cadastrada e seus atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,57 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+// crio uma function cadastrarCarta passando como propriedade o numero da carta que quero cadastrar, o mesmo sera tb mostrado ao exibir os valores cadastrados na carta
+
+void cadastrarCarta(int numeroCarta) {
+   // Área para definição das variáveis para armazenar as propriedades das cidade
+
+  char codigoEstado;
+  int codigoCidade;
+  char codigoCarta[4];
+  char nomeCidade[100];
+  int populacao;
+  float area;
+  float pib;
+  int pontosTuristicos;
 
   // Área para entrada de dados
+  printf("Para iniciar informe o código do Estado (Digite uma letra de A - H ): ");
+  scanf(" %c", &codigoEstado);
+  printf("Informe o código da cidade (1 a 4) : ");
+  scanf("%i", &codigoCidade);
+  snprintf(codigoCarta, sizeof(codigoCarta), "%c%02d", codigoEstado, codigoCidade);
+  printf("Informe o nome da cidade: ");
+  scanf(" %[^\n]", nomeCidade);
+  printf("Digite a população da cidade: ");
+  scanf("%d", &populacao);
+  printf("Digite a área da cidade (em km²): ");
+  scanf("%f", &area);
+  printf("Digite o PIB da cidade (em bilhões): ");
+  scanf("%f", &pib);
+  printf("Digite o número de pontos turísticos da cidade: ");
+  scanf("%d", &pontosTuristicos);
 
-  // Área para exibição dos dados da cidade
+    // Área para exibição dos dados da cidade
+  printf("\n--- Dados da Carta Cadastrada ---\n");
+  printf("Carta %d: \n", numeroCarta);
+  printf("Estado: %c\n", codigoEstado);
+  printf("Código: %s\n", codigoCarta);
+  printf("Nome da cidade: %s\n", nomeCidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %.f km²\n", area);
+  printf("PIB: %.f bilhões\n", pib);
+  printf("Pontos turísticos: %d\n", pontosTuristicos);
+}
 
+int main() {
+  printf("Bem-vindo ao jogo Super Trunfo com tema PAÍSES \n");
+
+  // chamo a function cadastrarCarta() e passo os valores pra primeira carta
+  cadastrarCarta(1);
+  printf("\n");
+  // chamada da função cadastrarCarta() com o numero da carta
+  cadastrarCarta(2);
+  printf("\n");
 return 0;
 } 
